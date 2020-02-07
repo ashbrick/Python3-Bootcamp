546e09b87234986b38a2277e51828dca489c9447
ashbrick <ashleycbrickhouse@gmail.com>
2020-02-07 14:08:25 -0500

    range practice

---
+++ loops/loop_practice.py
@@ -0,0 +1,19 @@
+# for loop syntax
+# for item in iterable_object:
+    # do something with item
+
+# for loops and ranges
+# for number in range(1, 8)
+#     print(number)
+    # will print numbers 1 - 7
+
+# for x in range(1, 10): # 1-9
+#     print(x)
+
+# for letter in "coffee":
+#     print(letter)
+
+# for letter in "coffee":
+#     print(letter * 10)
+
+
---
+++ loops/range.py
@@ -0,0 +1,17 @@
+# RANGES
+# range(7)      => gives you integers from 0 through 6 (count starts at 0 and is exclusive)
+# range(1,8)    => will give you integers from 1 to 7 (two parameters are (start, end))
+# range(1,10,2) => will give you odds from 1 to 10 (third param is called the "step", meaning how many to skip. Also which way to count, up + or down -)
+# range(7,0,-1) => will give you integers from 7 to 1
+
+# for num in range(10):
+#     print(num)
+
+# for num in range(0, 100, 5): # go from 0 - 99 in 5's 
+#     print(num)
+
+# for num in range(3,15,3):
+#     print(num)
+
+# for num in range(50,70):
+#     print(num) # prints all numbers from 50-69
